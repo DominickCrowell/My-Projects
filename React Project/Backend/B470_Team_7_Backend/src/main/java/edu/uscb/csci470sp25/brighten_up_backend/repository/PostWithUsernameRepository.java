3e1180b05bb62c5c4b1e76cb600dbeaf74e4b900
DominickCrowell <dominickcrowellus@gmail.com>
2025-06-12 16:26:53 -0400

    Delete React Project directory

--- React Project/Backend/B470_Team_7_Backend/src/main/java/edu/uscb/csci470sp25/brighten_up_backend/repository/PostWithUsernameRepository.java
+++
@@ -1,9 +0,0 @@
-package edu.uscb.csci470sp25.brighten_up_backend.repository;
-
-import edu.uscb.csci470sp25.brighten_up_backend.model.PostWithUsername;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface PostWithUsernameRepository extends JpaRepository<PostWithUsername, Long> {
-    Optional<PostWithUsername> findByPostId(Long postId);
-}
